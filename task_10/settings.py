96b887aa38b533ac4a068c3d7b5d30e4cd5aee21
kurohage <517326+kurohage@users.noreply.github.com>
2019-10-09 19:41:21 +0300

    task10 completed and passed tests finally!

--- task_10/settings.py
+++ task_10/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
